f2946ac9dd6a95abdb37495b2f4f46b7099b302b
M Ibad Guthwara <43487906+ibadguthwara@users.noreply.github.com>
2019-01-20 23:28:20 +0700

    Initial commit

---
+++ src/pbo3ulang/pkg10116049/latihan2/deklarasi/pengaksesanvariabel/PBO3ULANG10116049Latihan2DeklarasiPengaksesanVariabel.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3ulang.pkg10116049.latihan2.deklarasi.pengaksesanvariabel;
+
+/**
+ *
+ * @author ibadguthwara
+ * NAMA 	     : M. Ibad Guthwara
+ * KELAS	     : PBO 3 ULANG
+ * NIM		     : 10116049
+ * DESKRIPSI PROGRAM : Program ini berisi program untuk mendeklarasikan dan memberi nilai pada variabel
+ */
+public class PBO3ULANG10116049Latihan2DeklarasiPengaksesanVariabel {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // Deklarasi variabel
+        int nilaiInt;
+        final double PHI = 3.14; // Konstanta uppercase
+        boolean nilaiLogika;
+        char nilaiKarakter;
+        
+        // Memberi nilai ke variabel
+        nilaiInt = 78;
+        nilaiLogika = false;
+        nilaiKarakter = 'D';
+        
+        // Menampilkan hasil
+        System.out.println();
+        System.out.println("Isi variabel nilai = " + nilaiInt);
+        System.out.println("Isi variabel PHI = " + PHI);
+        System.out.println("Isi variabel logika = " + nilaiLogika);
+        System.out.println("Isi variabel karakter = " + nilaiKarakter);
+        
+    }
+    
+}
